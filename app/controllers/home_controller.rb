36b266be77daceac1523dfa0c90afa041a4b9d5d
Moncada <miguelmoncadag@gmail.com>
2018-09-19 13:17:46 -0600

    Filtros en la Busqueda

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,29 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
+
+    @order_by = params[:order_by]
+
+    @search = params[:search]
+
     @images = Image .paginate( page: params[:page] , per_page: 3)
                   .order(id: :desc)
                   .where private_img: true
+
+    if (  @search == "" && @order_by == "2" )
+      @images = Image .paginate( page: params[:page] , per_page: 3)
+                    .order(likes_counter: :desc)
+                    .where private_img: true
+    end
+
+    if ( @search != "" && @order_by == "1")
+      @images = Image.paginate( page: params[:page] , per_page: 3).order(id: :desc)
+                    .where("images.private_img = 1 AND images.title = ? OR images.description = ?", @search , @search)
+
+    end
+
+
+
 
     @user = User.find current_user.id
     @likes = @user.images
